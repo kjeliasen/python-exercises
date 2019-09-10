8e7b8a9c16898e34883e47587dca1cc921b17ac3
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-09-10 11:06:48 -0500

    Hello.py

---
+++ Hello.py
@@ -0,0 +1,8 @@
+print(1+1)
+print(2*3)
+print(5/2)
+print("Howdy, Y'all")
+
+# print("comments don't run")
+# Comments are good for communicating with people
+# We can 'comment out' a line of code to deactivate it
