4d7e941fbde57c0d913ab06f0a6fc5237f42cd39
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-09-12 16:57:17 -0500

    Trudging through problems

--- 4.4_function_exercises.py
+++ 4.4_function_exercises.py
@@ -8,37 +8,70 @@ Create a file named 4.4_function_exercises.py for this exercise. After creating 
 1.  Define a function named is_two. It should accept one input and return True if the passed input is either the number or the string 2, False otherwise.
 """
 
+def is_two(num):
+    return str(num) == 2
+
 """
 2.  Define a function named is_vowel. It should return True if the passed string is a vowel, False otherwise.
 """
+vowels = ['a','e','i','o','u']
+
+def is_vowel(char):
+    test = char[0].lower()
+    if test == 'y':
+        print('Maybe')
+
+    return test in vowels
 
 """
 3.  Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. Use your is_vowel function to accomplish this.
 """
 
+def is_consonant(char)
+    test = char[0].lower()
+    if test.isalpha():
+        if test =='y':
+            print('Maybe')
+        return test not in vowels
+    else:
+        return False
+
 """
 4.  Define a function that accepts a string that is a word. The function should capitalize the first letter of the word if the word starts with a consonant.
 """
+
+def return_word(chars)
+    
 
 """
 5.  Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
 """
 
+
+
 """
 6.  Define a function named apply_discount. It should accept a original price, and a discount percentage, and return the price after the discount is applied.
 """
+
+
 
 """
 7.  Define a function named handle_commas. It should accept a string that is a number that contains commas in it as input, and return a number as output.
 """
 
+
+
 """
 8.  Define a function named get_letter_grade. It should accept a number and return the letter grade associated with that number (A-F).
 """
 
+
+
 """
 9.  Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
 """
+
+
 
 """
 10. Define a function named normalize_name. It should accept a string and return a valid python identifier, that is:
@@ -52,20 +85,27 @@ Create a file named 4.4_function_exercises.py for this exercise. After creating 
         % Completed will become completed
 """
 
+
+
 """
 11. Write a function named cumsum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
         cumsum([1, 1, 1]) returns [1, 2, 3]
         cumsum([1, 2, 3, 4]) returns [1, 3, 6, 10]
 """
 
+
+
 """
 Bonii
 """
 
 
+
 """
 1.  Create a function named twelveto24. It should accept a string in the format 10:45am or 4:30pm and return a string that is the representation of the time in a 24-hour format. Bonus write a function that does the opposite.
 """
+
+
 
 
 """
