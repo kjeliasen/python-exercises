b5a0eb8ebe71e860c0e9eb92edb8625533a18d79
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-09-18 12:29:40 -0500

    loopy

--- checkbook.py
+++ checkbook.py
@@ -25,18 +25,18 @@ class UserExitException(Exception):
 def bug_note(func, verb='checking', **kwargs):
     print(f'DEBUG: {verb} fn({func})')
     if len(kwargs.items()):
-        print([f'{key} == {value}' for key, value in kwargs.items()])
+        print([f'{key.upper()} == {value}' for key, value in kwargs.items()])
 
 
 def load(file=entries_file_load):
     func = 'load'
-    bug_note(verb='starting', func=func)
+    # bug_note(verb='starting', func=func)
     with open(file) as fin:
         data = json.load(fin)
     got_users = data['users']
     got_accounts = data['accounts']
     got_entries = data['entries']
-    bug_note(verb='ending', func=func)
+    # bug_note(verb='ending', func=func)
     return got_users, got_accounts, got_entries
 
 
@@ -54,52 +54,59 @@ def save(users, accounts, entries, file=entries_file_save):
     return True
 
 
-def get_cur_user_info(context, users, current_user_info, **kwargs):
+def get_cur_user_info(context, users, **kwargs):
     func = 'get_cur_user_info'    
-    bug_note(verb='starting', func=func)
+    # bug_note(verb='starting', func=func)
     for user in users:
         if user['user_id'] == context['current_user_id']:
-            bug_note(verb='happily ending', func=func)
             current_user_info = user
+            context['current_user_info'] = current_user_info
+            # bug_note(verb='happily ending', func=func, user=user, current_user_info=context['current_user_info'])
             return True
-    bug_note(verb='unhappily ending', func=func)
     current_user_info = {
         'user_id': '0',
         'first_name': 'NO USER',
         'last_name': 'SELECTED'
     }
+    context['current_user_info'] = current_user_info
+    bug_note(verb='unhappily ending', func=func, current_user_info=context['current_user_info'])
     return False
 
 
-def get_cur_account_info(context, accounts, current_account_info, **kwargs):
+def get_cur_account_info(context, accounts, **kwargs):
     func = 'get_cur_account_info'    
-    bug_note(verb='starting', func=func)
+    # bug_note(verb='starting', func=func)
     for account in accounts:
         if account['account_id'] == context['current_account_id']:
-            bug_note(verb='happily ending', func=func)
             current_account_info = account
+            context['current_account_info'] = account
+            # bug_note(verb='happily ending', func=func, current_account_info=context['current_account_info'])
             return True
-    bug_note(verb='unhappliy ending', func=func)
     current_account_info = {
         'acct_id': '0',
         'acct_ref_name': 'NO ACCOUNT SELECTED'
     }
+    bug_note(verb='unhappliy ending', func=func, current_account_info=context['current_account_info'])
     return False
 
 
-def get_cur_user_accounts(context, accounts, current_users_accounts, **kwargs):
+def get_cur_user_accounts(context, accounts, **kwargs):
     func = 'get_cur_user_accounts'    
-    bug_note(verb='looping', func=func)
+    # bug_note(verb='looping', func=func)
+    # print(type(accounts))
     current_users_accounts = [account for account in accounts if account['account_id'] == context['current_user_id']]
-      
+    context['current_users_accounts'] = current_users_accounts
+    # bug_note(verb='ending', func=func, current_users_accounts=current_users_accounts)
+    return True
 
-def get_cur_account_entries(context, entries, current_entries, **kwargs):
+
+def get_cur_account_entries(context, entries, **kwargs):
     func = 'get_cur_account_entries'    
     bug_note(verb='looping', func=func)
-    return [entry for entry in entries if entry['account_id'] == context['current_user_id']]
+    return [entry for entry in entries if entry['account_id'] == context['current_account_id']]
     
 
-def cl_view_balance(context, current_entries, **kwargs):
+def cl_view_balance(context, entries, **kwargs):
     func = 'cl_view_balance'    
     bug_note(verb='starting', func=func)
     # do some of the things
@@ -123,27 +130,29 @@ def cl_record_credit(context, entries,  **kwargs):
     bug_note(verb='ending', func=func)
     
 
-def cl_change_user(context, users, accounts, current_user_info, current_users_accounts, **kwargs):
+def cl_change_user(context, users, accounts, **kwargs):
     func = 'cl_change_user'    
-    bug_note(verb='starting', func=func)
+    #bug_note(verb='starting', func=func)
     print('Changing User')
     # do some of the things
     context['current_user_id'] = users[0]['user_id']
-    get_cur_user_info(context, users, current_user_info)
-    get_cur_user_accounts(context, accounts, current_users_accounts)
-    bug_note(verb='ending', func=func, user_id = context['current_user_id'])
+    get_cur_user_info(context, users)
+    get_cur_user_accounts(context, accounts)
+    # bug_note(verb='ending', func=func, user_id = context['current_user_id'])
     
 
-def cl_change_account(context, accounts, current_users_accounts, **kwargs):
+def cl_change_account(context, accounts, entries, **kwargs):
     func = 'cl_change_account'    
-    bug_note(verb='starting', func=func)
+    #bug_note(verb='starting', func=func)
     # do some of the things
-    print('change amount')
-
-    bug_note(verb='ending', func=func)
+    print('Changing Account')
+    context['current_account_id'] = accounts[0]['account_id']
+    get_cur_account_info(context, accounts)
+    get_cur_account_entries(context, entries)
+    # bug_note(verb='ending', func=func, account_id = context['current_account_id'])
     
 
-def cl_get_users_info(context, users, current_user_info, **kwargs):
+def cl_get_users_info(context, users, **kwargs):
     func = 'cl_get_users_info'    
     bug_note(verb='starting', func=func)
     # do some of the things
@@ -151,7 +160,7 @@ def cl_get_users_info(context, users, current_user_info, **kwargs):
     bug_note(verb='ending', func=func)
     
 
-def cl_get_accounts_info(context, accounts, current_users_accounts, **kwargs):
+def cl_get_accounts_info(context, accounts, **kwargs):
     func = 'cl_get_accounts_info'    
     bug_note(verb='starting', func=func)
     # do some of the things
@@ -194,7 +203,7 @@ def cl_gtfo(context, **kwargs):
     raise UserExitException
 
 
-def unknown(context, **kwargs):
+def unknown(**kwargs):
     func = 'unknown'    
     bug_note(verb='encountering', func=func)
     print('wtf, chuck?')
@@ -230,7 +239,7 @@ def init_command_list():
         'X': (cl_gtfo, 'Exit')
         # ...
     }
-    bug_note(verb='ending', func=func)
+    # bug_note(verb='ending', func=func)
     return setcommands
 
 def init_context():
@@ -239,38 +248,51 @@ def init_context():
     setcontext = {
         'current_user_id': False,
         'current_account_id': False,
-        'data_dirty': False
+        'data_dirty': False,
+        'current_user_info': {},
+        'current_users_accounts': [],
+        'current_account_info': {},
+        'current_entries': {},
     } 
-    bug_note(verb='ending', func=func)
+    # bug_note(verb='ending', func=func)
     return setcontext              
 
 
 def main():
     func = 'main'    
-    bug_note(verb='starting', func=func)
+    # bug_note(verb='starting', func=func)
     commands = init_command_list()
     context = init_context()
     users, accounts, entries = load(entries_file_load)
-    current_user_info = {}
-    current_users_accounts = {}
-    current_account_info = {}
-    current_entries = {}
     do_command = False
     # cur_user_accounts = 
 
     try:
     # do all the things
+        print('\n\n')
         while True:
-            print('\n\n')
             if not context['current_user_id']:
-                print('DEBUG no current user')
+                #print('DEBUG no current user')
                 do_command = '4'
             else:
-                print('DEBUG found current user')
+                #print('DEBUG found current user')
                 #cur_user_info = get_cur_user_info(context['current_user_id'], users)
-                print('Current user is {} - {} {}'.format(cur_user_info['user_id'], cur_user_info['first_name'], cur_user_info['last_name']))
+                current_user_info = context['current_user_info']
+                #print(current_user_info)
+                print('Current user is {} - {} {}'.format(current_user_info['user_id'], 
+                    current_user_info['first_name'], current_user_info['last_name']))
                 #print(cur_user_info)
-                pass
+                if not context['current_account_id']:
+                    #print('DEBUG no current account')
+                    do_command = '5'
+                else:
+                    #print('DEBUG found current account')
+                    #cur_user_info = get_cur_user_info(context['current_user_id'], users)
+                    current_account_info = context['current_account_info']
+                    #print(current_account_info)
+                    print('Current account is {} - {} labeled \'{}\''.format(current_account_info['account_id'], 
+                        current_account_info['account_type'], current_account_info['account_ref_name']))
+                    #print(cur_user_info)                
             if not do_command:
                 print('\n\n')
                 for command, (fn, desc) in commands.items():
@@ -282,12 +304,13 @@ def main():
             print(f'DEBUG: Selected fn == {fn[1]}')
             fn = fn[0]
             fn(
-                context=context, users=users, accounts=accounts, entries=entries,
-                current_user_info=current_user_info, current_users_accounts=current_users_accounts,
-                current_account_info=current_account_info, current_entries=current_entries
+                context=context, users=users, accounts=accounts, entries=entries #,
+                #current_user_info=current_user_info, current_users_accounts=current_users_accounts,
+                #current_account_info=current_account_info, current_entries=current_entries
             )
             do_command = False
             bug_note(verb='reiterating', func=func)
+            #bug_note(verb='reiterating', func=func, current_user_info=context['current_user_info'], current_account_info=context['current_account_info'])
             
     except Exception as e:
         print('EXCEPTION RAISED')
