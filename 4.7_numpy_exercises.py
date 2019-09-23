9aa5b4f7de56989fa4f1dba9806d17f36d7ef94f
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-09-23 10:39:01 -0500

    Numpy Exercises - cleaned up

--- 4.7_numpy_exercises.py
+++ 4.7_numpy_exercises.py
@@ -8,37 +8,61 @@ import numpy as np
 """
 Use the following code for the questions below:
 """
-a = np.array([4, 10, 12, 23, -2, -1, 0, 0, 0, -6, 3, -7])
 
-"""
-How many negative numbers are there?
-"""
+
+a = np.array([4, 10, 12, 23, -2, -1, 0, 0, 0, -6, 3, -7])
+print(f'Original array a: ')
+
+
+# Make Functions
+
+
 def get_negatives(array):
     return array[array < 0]
 
-print(f'Negatives: {len(get_negatives(a))}')
 
-"""
-How many positive numbers are there?
-"""
 def get_positives(array):
     return array[array > 0]
 
-print(f'Positives: {len(get_positives(a))}')
 
-"""
-How many even positive numbers are there?
-"""
 def get_odds(array):
     return array[array % 2 == 1]
+
 
 def get_evens(array):
     return array[array % 2 == 0]
 
+
 def get_even_positives(array):
     return get_positives(get_evens(array))
 
+
+def centered(array):
+    return array - array.mean()
+
+
+def get_z_score(array):
+    return centered(array) / array.std()
+
+
+"""
+How many negative numbers are there?
+"""
+print(f'The {len(get_negatives(a))} negatives in a: {get_negatives(a)}')
+
+
+"""
+How many positive numbers are there?
+"""
+print(f'The {len(get_positives(a))} positives in a: {get_positives(a)}')
+
+
+"""
+How many even positive numbers are there?
+"""
+
 print(f'Even positives: {len(get_even_positives(a))}')
+
 
 """
 If you were to add 3 to each data point, how many positive numbers would there be?
@@ -60,11 +84,6 @@ the data such that the center of the data is at 0. This is done by subtracting
 the mean from each data point. Center the data set.
 """
 
-
-def centered(array):
-    return array - array.mean()
-
-
 print(f'Centered a: {centered(a)}')
 
 """
@@ -72,11 +91,6 @@ Calculate the z-score for each data point. Recall that the z-score is given by:
 
 Z = x − μ / σ
 """
-
-
-def get_z_score(array):
-    return centered(array) / array.std()
-
 
 print(f'Z-scores for a: {get_z_score(a)}')
 
