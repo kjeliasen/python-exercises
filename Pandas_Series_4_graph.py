2dbbe3c30786c0c292cb322f23e7615e4055946d
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-09-25 11:12:45 -0500

    Pandas_Series code golf

--- Pandas_Series_4_graph.py
+++ Pandas_Series_4_graph.py
@@ -2,8 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # ## 4. Use pandas to create a Series from the following string:
 # 
-# `'hnvidduckkqxwymbimkccexbkmqygkxoyndmcxnwqarhyffsjpsrabtjzsypmzadfavyrnndndvswreauxovncxtwzpwejilzjrmmbbgbyxvjtewqthafnbkqplarokkyydtubbmnexoypulzwfhqvckdpqtpoppzqrmcvhhpwgjwupgzhiofohawytlsiyecuproguy'`
-
+# 'hnvidduckkqxwymbimkccexbkmqygkxoyndmcxnwqarhyffsjpsrabtjzsypmzadfavyrnndndvswreauxovncxtwzpwejilzjrmmbbgbyxvjtewqthafnbkqplarokkyydtubbmnexoypulzwfhqvckdpqtpoppzqrmcvhhpwgjwupgzhiofohawytlsiyecuproguy'
+#
+# Create a bar plot of the frequencies of the 6 most frequently occuring letters.
 Letters = pd.Series(list('hnvidduckkqxwymbimkccexbkmqygkxoyndmcxnwqarhyffsjpsrabtjzsypmzadfavyrnndndvswreauxovncxtwzpwejilzjrmmbbgbyxvjtewqthafnbkqplarokkyydtubbmnexoypulzwfhqvckdpqtpoppzqrmcvhhpwgjwupgzhiofohawytlsiyecuproguy'))
 Letters_list = Letters.value_counts()
 Letters_list[Letters_list >= Letters_list[5]].plot.bar()
