df779d4e1794bc63ea2c0819e505db8ef506694e
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-09-25 11:11:35 -0500

    Pandas_Series code golf

---
+++ Pandas_Series_4_graph.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+# ## 4. Use pandas to create a Series from the following string:
+# 
+# `'hnvidduckkqxwymbimkccexbkmqygkxoyndmcxnwqarhyffsjpsrabtjzsypmzadfavyrnndndvswreauxovncxtwzpwejilzjrmmbbgbyxvjtewqthafnbkqplarokkyydtubbmnexoypulzwfhqvckdpqtpoppzqrmcvhhpwgjwupgzhiofohawytlsiyecuproguy'`
+
+Letters = pd.Series(list('hnvidduckkqxwymbimkccexbkmqygkxoyndmcxnwqarhyffsjpsrabtjzsypmzadfavyrnndndvswreauxovncxtwzpwejilzjrmmbbgbyxvjtewqthafnbkqplarokkyydtubbmnexoypulzwfhqvckdpqtpoppzqrmcvhhpwgjwupgzhiofohawytlsiyecuproguy'))
+Letters_list = Letters.value_counts()
+Letters_list[Letters_list >= Letters_list[5]].plot.bar()
+plt.show()
